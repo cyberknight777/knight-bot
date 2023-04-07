7d75fdd635c10bbea75e6e78c7d3484b5bf43566
Cyber Knight <cyberknight755@gmail.com>
2023-04-07 17:28:36 +0800

    src: Make help message detailed
    - I use gramme.rs not teloxide.
    - Also add /link and /urb.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- src/plugins/help.rs
+++ src/plugins/help.rs
@@ -10,16 +10,18 @@ type Result = std::result::Result<(), Box<dyn std::error::Error>>;
 
 pub async fn knightcmd_help(message: Message) -> Result {
     let msg = "Hello There!, \
-	I am a bot made by cyberknight777 in Rust based on teloxide.\n\
+	I am a bot made by cyberknight777 in Rust based on gramme.rs.\n\
 	Here's a list of my commands:-\n\
 	/eightball ~ Rolls an eightball to say yes/no.\n\
-        /flipcoin ~ Flips a coin to say heads/tails.\n\
+	/flipcoin ~ Flips a coin to say heads/tails.\n\
 	/help ~ Displays this text.\n\
+	/link [url] ~ Gets the last redirected URL.\n\
 	/l ~ To say your lucky number.\n\
-	/msg ~ Sends text.\n\
+	/msg [msg] ~ Sends text.\n\
 	/neo ~ Sends neofetch output. \n\
 	/ping ~ Checks how fast I can respond.\n\
-	/start ~ Checks if I'm alive.\n";
+	/start ~ Checks if I'm alive.\n\
+	/urb [word] ~ Gets definition of word from urban dictionary.\n";
     message.reply(msg).await?;
     return Ok(());
 }
