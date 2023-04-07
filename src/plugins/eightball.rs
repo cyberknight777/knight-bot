ea4af8bc6dd59846106f59cfc5a8a94e68127920
Cyber Knight <cyberknight755@gmail.com>
2023-04-07 16:43:56 +0800

    Revert "src: Drop redundant client argument for relevant modules"
    - This reverts commit 9e21e960b24116e955cb03e6daf5539bc19c7a92 as we have usecases for it now.

--- src/plugins/eightball.rs
+++ src/plugins/eightball.rs
@@ -5,11 +5,11 @@
 //!
 
 use crate::plugins;
-use grammers_client::types::Message;
+use grammers_client::{Client, types::Message};
 
 type Result = std::result::Result<(), Box<dyn std::error::Error>>;
 
-pub async fn knightcmd_eightball(message: Message) -> Result {
+pub async fn knightcmd_eightball(_client: Client, message: Message) -> Result {
     let ball = plugins::random(2);
     let result = if ball == 0 { "Yes, it is the truth!" } else { "No, this is a prepostrous lie!" };
     message.reply(result).await?;
--- src/plugins/luck.rs
+++ src/plugins/luck.rs
@@ -5,11 +5,14 @@
 //!
 
 use crate::plugins;
-use grammers_client::types::{InputMessage, Message};
+use grammers_client::{
+    Client,
+    types::{InputMessage, Message}
+};
 
 type Result = std::result::Result<(), Box<dyn std::error::Error>>;
 
-pub async fn knightcmd_luck(message: Message) -> Result {
+pub async fn knightcmd_luck(_client: Client, message: Message) -> Result {
     let random_number = plugins::random(101); // modulo 101 to get a number between 0 to 100
     message.reply(InputMessage::html(format!("Your lucky number is: <code>{}</code>", random_number))).await?;
 
