3cb68ba43ecc0069002b61e2c692cb753c92ee73
Cyber Knight <cyberknight755@gmail.com>
2023-04-07 19:24:30 +0800

    src: Implement error handling for msg
    - It should send a message and bail out when the parameter is empty.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- src/plugins/msg.rs
+++ src/plugins/msg.rs
@@ -12,6 +12,10 @@ use grammers_client::{
 type Result = std::result::Result<(), Box<dyn std::error::Error>>;
 
 pub async fn knightcmd_msg(client: Client, message: Message, text: String) -> Result {
+     if text.trim().is_empty() {
+	message.reply(InputMessage::html("Send what? Give me <b>any text</b> to send!")).await?;
+	return Ok(());
+    }
     if let Some(id) = message.reply_to_message_id() {
 	client.send_message(message.chat(), InputMessage::markdown(text.trim().replace(r#"\n"#, "  \n")).reply_to(Some(id))).await?;
     } else {
