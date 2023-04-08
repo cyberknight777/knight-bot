6c08106e4febf64b8444496103407b2e9656acb1
Cyber Knight <cyberknight755@gmail.com>
2023-04-08 19:47:25 +0800

    src: Add help functions of several commands
    - To maintain consistency.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- src/plugins/help.rs
+++ src/plugins/help.rs
@@ -12,17 +12,25 @@ pub async fn knightcmd_help(message: Message) -> Result {
     let msg = "Hello There!, \
 	I am a bot made by cyberknight777 in Rust based on gramme.rs.\n\
 	Here's a list of my commands:-\n\
+	/aur [package] ~ Gets package information from AUR.\n\
+	/cat [http code] ~ Sends cat pic according to http codes.\n\
+	/dog [http code] ~ Sends dog pic according to http codes.\n\
 	/eightball ~ Rolls an eightball to say yes/no.\n\
 	/flipcoin ~ Flips a coin to say heads/tails.\n\
 	/help ~ Displays this text.\n\
+	/ipa [ip] ~ Gets information of given IP.\n\
 	/link [url] ~ Gets the last redirected URL.\n\
 	/l ~ To say your lucky number.\n\
+	/man [command] ~ Gets information of a command from manpages.\n\
 	/msg [msg] ~ Sends text.\n\
 	/neo ~ Sends neofetch output. \n\
 	/ping ~ Checks how fast I can respond.\n\
+	/plant [http code] ~ Sends plant pic according to http codes.\n\
+	/run ~ Runnns :).\n\
 	/start ~ Checks if I'm alive.\n\
 	/uid ~ Gets UserID & ChatID>.\n\
-	/urb [word] ~ Gets definition of word from urban dictionary.\n";
+	/urb [word] ~ Gets definition of word from urban dictionary.\n\
+	/whois [site] ~ Gets WHOIS information of site.";
     message.reply(msg).await?;
     return Ok(());
 }
