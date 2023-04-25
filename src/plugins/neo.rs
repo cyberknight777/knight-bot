6e542b62df8af0e523a55b972fea23450f4fae44
Cyber Knight <cyberknight755@gmail.com>
2023-04-26 00:41:41 +0800

    src: Use neofetch instead of osfetch-rs
    - Neofetch is more widely used anyways.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- src/plugins/neo.rs
+++ src/plugins/neo.rs
@@ -12,7 +12,7 @@ use std::process::Command;
 type Result = std::result::Result<(), Box<dyn std::error::Error>>;
 
 pub async fn knightcmd_neo(client: Client, message: Message) -> Result {
-    let neofetch = Command::new("osfetch-rs").arg("--stdout").output().expect("Failed to execute command!");
+    let neofetch = Command::new("neofetch").arg("--stdout").output().expect("Failed to execute command!");
     let text = String::from_utf8_lossy(&neofetch.stdout).to_string();
     let input_message = InputMessage::html(format!("<code>{}</code>", text.trim()));
     client.send_message(message.chat(), input_message).await?;
